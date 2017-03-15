66d70f8de5cc7751b640ea9855d04f369b31cde1
Jennifer Oakes <jenniferoakes@Jennifers-MacBook-Pro.local>
2017-03-15 13:38:40 -0400

    Added test to ensure that the sum function is actually a function *sanity test*

--- test/specs/calc.spec.js
+++ test/specs/calc.spec.js
@@ -5,9 +5,19 @@
 
   describe('calc module', function() {
     describe('sum function', function() {
+
       it('should expect that 1 equals 1', function () {
         expect(1).to.equal(1);
       });
+
+      it('should be a function', function() {
+        let result = window.calc.sum;
+        expect( result ).to.be.a('function');
+      });
+
+      
+
+
     });
 
     describe('factorial function', function() {
