00fff6798d711b730292127da008823fe1189cf5
Jennifer Oakes <jenniferoakes@Jennifers-MacBook-Pro.local>
2017-03-15 13:30:43 -0400

    Completed one successful test to show that the sum function does infact recognize numbers *sanity check*

--- test/specs/calc.spec.js
+++ test/specs/calc.spec.js
@@ -1,10 +1,24 @@
 (function() {
-    'use strict';
+  'use strict';
+
+  let expect = chai.expect;
+
+  describe('calc module', function() {
+    describe('sum function', function() {
+      it('should expect that 1 equals 1', function () {
+        expect(1).to.equal(1);
+      });
+    });
+
+    describe('factorial function', function() {
+
+    });
 
 
-    // TODO: Your tests go here!
 
-    
+  });
+
+
 
 
 })();
